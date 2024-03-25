d99d26fa769a8b1e127c6400751eab98fe25456e
Cees Timmerman <CTimmerman@users.noreply.github.com>
2024-03-25 04:21:37 +0100

    Red toasts for errors. More cz-like commit-msg pattern.

--- dev_setup.bat
+++ dev_setup.bat
@@ -1,3 +1,3 @@
 pip install -r requirements-dev.txt
 pre-commit install
-REM python -c "print('''#!/bin/env python\nimport re, sys; pat = '^(bug|feat)';\nif re.match(pat, open(sys.argv[1], 'r').read()): sys.exit(0)\nelse: print(f'Commit message doesn\\'t match {pat}'); sys.exit(1)''')" > .git/hooks/commit-msg
+REM python -c "print('''#!/bin/env python\nimport re, sys; pat = '^(fix|feat):';\nif re.match(pat, open(sys.argv[1], 'r').read()): sys.exit(0)\nelse: print(f'Commit message doesn\\'t match {pat}'); sys.exit(1)''')" > .git/hooks/commit-msg
