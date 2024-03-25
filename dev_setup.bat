0f3652eb31ac713f804822c6a599d3ebf2b2f729
Cees Timmerman <CTimmerman@users.noreply.github.com>
2024-03-25 02:37:26 +0100

    feat:Trying a commit-msg hook.

--- dev_setup.bat
+++ dev_setup.bat
@@ -1,3 +1,3 @@
 pip install -r requirements-dev.txt
 pre-commit install
-pre-commit run --all-files
+python -c "print('''#!/bin/env python\nimport re, sys; sys.exit(0 if re.match('^(bug|feat)', open(sys.argv[1], 'r').read()) else 1)''')" > .git/hooks/commit-msg
