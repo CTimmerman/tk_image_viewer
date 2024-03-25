444606ef9e1aaabe7607cfa300dc15ca743e3e89
Cees Timmerman <CTimmerman@users.noreply.github.com>
2024-03-25 03:00:49 +0100

    No need to indent or check commit message.

--- dev_setup.bat
+++ dev_setup.bat
@@ -1,3 +1,3 @@
 pip install -r requirements-dev.txt
 pre-commit install
-python -c "print('''#!/bin/env python\nimport re, sys; pat = '^(bug|feat)';\nif re.match(pat, open(sys.argv[1], 'r').read()):\n\tsys.exit(0)\nelse: print(f'Commit message doesn\\'t match {pat}'); sys.exit(1)''')" > .git/hooks/commit-msg
+REM python -c "print('''#!/bin/env python\nimport re, sys; pat = '^(bug|feat)';\nif re.match(pat, open(sys.argv[1], 'r').read()): sys.exit(0)\nelse: print(f'Commit message doesn\\'t match {pat}'); sys.exit(1)''')" > .git/hooks/commit-msg
