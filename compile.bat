639dd6fc6095c20f72e0f45b783f1edaea9271e1
Cees Timmerman <CTimmerman@users.noreply.github.com>
2025-01-08 19:52:23 +0100

    Compiled release using Nuitka.

---
+++ compile.bat
@@ -0,0 +1,6 @@
+rem needs MSVC14 from Visual Studio Build Tools 2022. https://visualstudio.microsoft.com/visual-cpp-build-tools/
+rem build\__native.c(18944): error C2065: 'CPyStatic_Fits___ALL': undeclared identifier
+rem mypyc main.py
+
+rem pip install nuitka
+nuitka main.py --output-dir=nuitka-dist --standalone --onefile --output-filename=tiv.exe --windows-icon-from-ico=eye.ico --macos-app-icon=eye.ico --linux-icon=eye.ico --assume-yes-for-downloads --enable-plugin=tk-inter --deployment
