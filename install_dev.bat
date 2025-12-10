f4c13950149b0734b79743f823f191d1fbf580c6
Cees Timmerman <CTimmerman@users.noreply.github.com>
2025-12-10 14:06:50 +0100

    Also upgrade dev deps

--- install_dev.bat
+++ install_dev.bat
@@ -1,3 +1,3 @@
-pip install -r requirements-dev.txt
+pip install -r requirements-dev.txt --upgrade
 pre-commit install
 REM python -c "print('''#!/bin/env python\nimport re, sys; pat = '^(fix|feat):';\nif re.match(pat, open(sys.argv[1], 'r').read()): sys.exit(0)\nelse: print(f'Commit message doesn\\'t match {pat}'); sys.exit(1)''')" > .git/hooks/commit-msg
